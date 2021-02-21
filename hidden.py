3db65d7eaf0250c6626c63621a9b4a8b553a4820
Glib Solodzhuk <glibesyck@MacBook-Pro-Glib.local>
2021-02-21 10:54:39 +0200

    hiding my keys

--- hidden.py
+++ hidden.py
@@ -6,5 +6,4 @@ def oauth ():
     '''
     Return Bearer token to get Twitter API.
     '''
-    return "AAAAAAAAAAAAAAAAAAAAAGWRMwEAAAAA3xm5fp8cwgvO5UPrHmB1\
-d64VUb4%3DXYlJuFIn3uujbHIpB1RJHYFbjMdukVi4jb4vRFsjinsS8dm4uA" #put your bearer token here
+    return "your bearer token" #put your bearer token here
